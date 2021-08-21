fa63c621f3b5cf40dccda2e85efd707ceff87257
kyohei_i <kyoheishogun.1997@i.softbank.jp>
2021-08-21 21:42:00 +0900

    gitignore追加と新規アプリ追加

---
+++ Kadai8-MVVM/Kadai8-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kadai8-MVVM
+//
+//  Created by 今村京平 on 2021/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
